81f863cc0677ba0685a0b087c5b9dc42f07b0a8e
gijoe2day <gjuliao32@hotmail.com>
2020-07-13 18:32:45 -0500

    first version

---
+++ app.js
@@ -0,0 +1,85 @@
+var myApp = angular.module("myApp", ["ngRoute", "ngAnimate"]);
+
+myApp.config([
+  "$routeProvider",
+  "$locationProvider",
+  function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when("/home", {
+        templateUrl: "views/home.html",
+        controller: "NinjaController",
+      })
+      .when("/contact", {
+        templateUrl: "views/contact.html",
+        controller: "ContactController",
+      })
+      .when("/contact-success", {
+        templateUrl: "views/contact-success.html",
+        controller: "ContactController",
+      })
+      .when("/directory", {
+        templateUrl: "views/directory.html",
+        controller: "NinjaController",
+      })
+      .otherwise({
+        redirectTo: "/home",
+      });
+  },
+]);
+
+myApp.directive("randomNinja", [
+  function () {
+    return {
+      restrict: "E",
+      scope: {
+        ninjas: "=",
+        title: "=",
+      },
+      templateUrl: "views/random.html",
+      transclude: true,
+      replace: true,
+      controller: function ($scope) {
+        $scope.random = Math.floor(Math.random() * 4);
+      },
+    };
+  },
+]);
+
+myApp.controller("NinjaController", [
+  "$scope",
+  "$http",
+  function ($scope, $http) {
+    $scope.removeNinja = function (ninja) {
+      var removeNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removeNinja, 1);
+    };
+
+    $scope.addNinja = function () {
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        available: true,
+      });
+
+      $scope.newninja.name = "";
+      $scope.newninja.belt = "";
+      $scope.newninja.rate = "";
+    };
+    $http.get("data/ninjas.json").then(function (res) {
+      $scope.ninjas = res.data;
+    });
+  },
+]);
+
+myApp.controller("ContactController", [
+  "$scope",
+  "$location",
+  function ($scope, $location) {
+    $scope.sendMessage = function () {
+      $location.path("#!/contact-success");
+    };
+  },
+]);
